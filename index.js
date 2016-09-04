707898f5651c69c03a1fba444354991b0cbb2ccb
Anthony Gore <anthony@magnum.technology>
2016-09-04 21:11:22 +1000

    Adding AMD wrapper

--- index.js
+++ index.js
@@ -1,109 +1,111 @@
-
-
 "use strict";
 
-module.exports = function(el) {
-
-	// Create and attach the iframe
-	this.iframe = document.createElement('iframe');
-	parentElement.appendChild(el);
+define(function (require, exports, module) {
 	
-	// Write to the iframe
-	this.write = (html) => {
-		this.iframe.contentWindow.document.open("text/html", "replace");
-		this.iframe.contentWindow.document.write(html); // Rendering starts during/after the write() command (depending on browser)
-		this.iframe.contentWindow.document.close(); // Rendering is not necessarily finished when close() is called
-		return this;
-	};
+	module.exports = function(el) {
 
-	this.state = '';
+		// Create and attach the iframe
+		this.iframe = document.createElement('iframe');
+		parentElement.appendChild(el);
 
-	this.html = (html) => {
-		if (typeof(html) === 'undefined') {
-			return this.state;
-		}
-		this.state = html;
-		return this;
-	};
+		// Write to the iframe
+		this.write = (html) => {
+			this.iframe.contentWindow.document.open("text/html", "replace");
+			this.iframe.contentWindow.document.write(html); // Rendering starts during/after the write() command (depending on browser)
+			this.iframe.contentWindow.document.close(); // Rendering is not necessarily finished when close() is called
+			return this;
+		};
 
-	this.setAttributes = (elem, attrs) => {
-		for (var key in attrs) {
-			elem.setAttribute(key, attrs[key]);
-		}
-		return elem;
-	}
+		this.state = '';
 
-	this.appendElementHead = (elem) => {
-		var dummy = document.createElement('html');
-		dummy.innerHTML = this.state;
-		dummy.getElementsByTagName('head')[0].appendChild(elem);
-		this.state = dummy.innerHTML;
-		return this;
-	}
+		this.html = (html) => {
+			if (typeof(html) === 'undefined') {
+				return this.state;
+			}
+			this.state = html;
+			return this;
+		};
 
-	this.appendElementBody = (elem) => {
-		var dummy = document.createElement('html');
-		dummy.innerHTML = this.state;
-		dummy.getElementsByTagName('body')[0].appendChild(elem);
-		this.state = dummy.innerHTML;
-		return this;
-	}
+		this.setAttributes = (elem, attrs) => {
+			for (var key in attrs) {
+				elem.setAttribute(key, attrs[key]);
+			}
+			return elem;
+		};
 
-	// Add script to HTML
-	this.addScript = (scriptContent, scriptAttributes, isBody) => {
-		// Create script element
-		var script = document.createElement('script');
-		script = setAttributes(script, scriptAttributes);
-		if (scriptContent != null) {
-			script.textContent = scriptContent;
-		}
-		if (isBody) {
-			return appendElementBody(script)
-		} else {
-			return appendElementHead(script)
-		}
-	}
+		this.appendElementHead = (elem) => {
+			var dummy = document.createElement('html');
+			dummy.innerHTML = this.state;
+			dummy.getElementsByTagName('head')[0].appendChild(elem);
+			this.state = dummy.innerHTML;
+			return this;
+		};
 
-	// Add script to head
-	this.addScriptHead = (scriptContent, scriptAttributes) => {
-		if (typeof scriptAttributes === "undefined") {
-			scriptAttributes = {};
-		}
-		return addScript(scriptContent, scriptAttributes, false);
-	};
+		this.appendElementBody = (elem) => {
+			var dummy = document.createElement('html');
+			dummy.innerHTML = this.state;
+			dummy.getElementsByTagName('body')[0].appendChild(elem);
+			this.state = dummy.innerHTML;
+			return this;
+		};
 
-	// Add script to body
-	this.addScriptBody = (scriptContent, scriptAttributes) => {
-		if (typeof scriptAttributes === "undefined") {
-			scriptAttributes = {};
-		}
-		return addScript(scriptContent, scriptAttributes, true);
-	};
+		// Add script to HTML
+		this.addScript = (scriptContent, scriptAttributes, isBody) => {
+			// Create script element
+			var script = document.createElement('script');
+			script = setAttributes(script, scriptAttributes);
+			if (scriptContent != null) {
+				script.textContent = scriptContent;
+			}
+			if (isBody) {
+				return appendElementBody(script)
+			} else {
+				return appendElementHead(script)
+			}
+		};
 
-	// Add style
-	this.addStyle = (cssString) => {
-		// Create style element
-		var style = document.createElement('style');
-		style.textContent = cssString;
-		return appendElementHead(style)
-	};
+		// Add script to head
+		this.addScriptHead = (scriptContent, scriptAttributes) => {
+			if (typeof scriptAttributes === "undefined") {
+				scriptAttributes = {};
+			}
+			return addScript(scriptContent, scriptAttributes, false);
+		};
 
-	// Find and replace
-	this.findAndReplace = (tag, callback) => {
-		var dummy = document.createElement('html');
-		dummy.innerHTML = this.state;
-		var collection = dummy.getElementsByTagName(tag);
-		for (var index in collection) {
-			if (collection.hasOwnProperty(index)) {
-				if (index !== 'length') {
-					callback(collection[index]);
+		// Add script to body
+		this.addScriptBody = (scriptContent, scriptAttributes) => {
+			if (typeof scriptAttributes === "undefined") {
+				scriptAttributes = {};
+			}
+			return addScript(scriptContent, scriptAttributes, true);
+		};
+
+		// Add style
+		this.addStyle = (cssString) => {
+			// Create style element
+			var style = document.createElement('style');
+			style.textContent = cssString;
+			return appendElementHead(style)
+		};
+
+		// Find and replace
+		this.findAndReplace = (tag, callback) => {
+			var dummy = document.createElement('html');
+			dummy.innerHTML = this.state;
+			var collection = dummy.getElementsByTagName(tag);
+			for (var index in collection) {
+				if (collection.hasOwnProperty(index)) {
+					if (index !== 'length') {
+						callback(collection[index]);
+					}
 				}
 			}
-		}
-		this.state = dummy.innerHTML;
+			this.state = dummy.innerHTML;
+			return this;
+		};
+
 		return this;
-	}
 
-	return this;
+	};
 
-};
+});
